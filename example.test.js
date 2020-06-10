1a5c5b9de7c92979f0b9b3f556bd388ae70c9502
Lidii <lidiiany92@gmail.com>
2020-06-09 17:29:25 -0700

    added feedback test

--- example.test.js
+++ example.test.js
@@ -1,17 +1,20 @@
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
+import FeedbackPage from "./pages/FeedbackPage";
 import TopBar from "./pages/components/TopBar";
 
 describe("Example", () => {
   let homepage;
   let topBar;
   let loginPage;
+  let feedbackPage;
 
   beforeAll(async () => {
     jest.setTimeout(20000);
     homepage = new HomePage();
     topBar = new TopBar();
     loginPage = new LoginPage();
+    feedbackPage = new FeedbackPage();
   });
 
   it("should open the home page", async () => {
@@ -29,5 +32,14 @@ describe("Example", () => {
     await loginPage.login("username", "password");
   });
 
-  it.only("feedback test", async () => {});
+  it.only("feedback test", async () => {
+    feedbackPage.visit();
+    feedbackPage.isFeedbackFormDisplayed();
+    feedbackPage.submitFeedback(
+      "test",
+      "testemail@gmail.com",
+      "test subject",
+      "test test test"
+    );
+  });
 });
