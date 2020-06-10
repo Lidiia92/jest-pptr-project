4ed91ba9470b582278ab775fee686a6f9f209778
Lidii <lidiiany92@gmail.com>
2020-06-09 18:03:51 -0700

    refactored to e2e testing

---
+++ tests/end-to-end.test.js
@@ -0,0 +1,43 @@
+import HomePage from "../pages/HomePage";
+import LoginPage from "../pages/LoginPage";
+import FeedbackPage from "../pages/FeedbackPage";
+import TopBar from "../pages/components/TopBar";
+
+describe("End to End testing", () => {
+  let homePage;
+  let topBar;
+  let loginPage;
+  let feedbackPage;
+
+  beforeAll(async () => {
+    jest.setTimeout(15000);
+    homePage = new HomePage();
+    topBar = new TopBar();
+    loginPage = new LoginPage();
+    feedbackPage = new FeedbackPage();
+  });
+
+  it("should load the home page", async () => {
+    await homePage.visit();
+    await topBar.isTopBarDisplayed();
+  });
+
+  it("should submit feedback", async () => {
+    await homePage.clickFeedbackLink();
+    await feedbackPage.isFeedbackFormDisplayed();
+    await feedbackPage.submitFeedback(
+      "test",
+      "testemail@gmail.com",
+      "test subject",
+      "test test test"
+    );
+  });
+
+  it("should login into application", async () => {
+    await homePage.visit();
+    await topBar.isTopBarDisplayed();
+    await topBar.clickSignInButton();
+    await loginPage.isLoginFormDisplayed();
+    await loginPage.login("username", "password");
+  });
+});
